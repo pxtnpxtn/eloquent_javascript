dab4bddfbeb93ffe79af85750de518d5f12b8db7
Paxton <paxton@Paxtons-MacBook-Pro.local>
2020-05-26 23:55:15 -0400

    C1: Q1, Q2

--- chapter_1.js
+++ chapter_1.js
@@ -1,2 +1,16 @@
 // Chapter 1 Projects
-// Test test
+// 1). Looping a triangle
+let triangleCount = '';
+
+while (triangleCount.length < 7) {
+    console.log(triangleCount);
+    triangleCount = triangleCount + '#';
+}
+
+// 2). FizzBuzz
+let fizzCount = '';
+
+for (let i = 1; i <= 100; i++) {
+    fizzCount = ((i % 3 === 0 ? 'Fizz' : '') + (i % 5 === 0 ? 'Buzz' : '') || i)
+    console.log(fizzCount);
+}
