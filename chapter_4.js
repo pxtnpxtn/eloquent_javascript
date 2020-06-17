7356dcf49dddc7d7b44022d575bcced0d6404e12
Paxton <paxton@Paxtons-MacBook-Pro.local>
2020-06-16 23:25:04 -0400

    C4: Q1

---
+++ chapter_4.js
@@ -0,0 +1,41 @@
+// Chapter 4 Projects
+// 1). The Sum of a Range
+function range(start, end, step) {
+    const arr = [];
+
+    while(start <= end) {
+        arr.push(start);
+        start+=step;
+    }
+    console.log(arr);
+}
+
+range(1, 10, 3);
+
+function sum(arr) {
+    let summed = 0;
+
+    arr.forEach(element => {
+        summed += element;
+    });
+    
+    console.log(summed);
+}
+
+// sum([1,2,4,3,10]);
+
+// 2). Reversing an Array
+
+// function reverseArray(arr) {
+//     console.log(arr);
+// }
+
+// reverseArray([1,3,3,7]);
+
+const arr = ["1", "2", "3", "4", "5"];
+
+const total = arr.reduceRight((prev, cur) => {
+    return prev + cur;
+});
+
+console.log(total);
