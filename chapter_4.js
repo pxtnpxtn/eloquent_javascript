a85aa869e98abaabecdee718a1576bed4cd129f6
Paxton <paxton@Paxtons-MacBook-Pro.local>
2020-07-04 16:05:08 -0400

    C4: Q2

--- chapter_4.js
+++ chapter_4.js
@@ -1,41 +1,45 @@
 // Chapter 4 Projects
 // 1). The Sum of a Range
-function range(start, end, step) {
-    const arr = [];
+// function range(start, end, step) {
+//     const arr = [];
 
-    while(start <= end) {
-        arr.push(start);
-        start+=step;
-    }
-    console.log(arr);
-}
+//     while(start <= end) {
+//         arr.push(start);
+//         start+=step;
+//     }
+//     console.log(arr);
+// }
 
-range(1, 10, 3);
+// range(1, 10, 3);
 
-function sum(arr) {
-    let summed = 0;
+// function sum(arr) {
+//     let summed = 0;
 
-    arr.forEach(element => {
-        summed += element;
-    });
+//     arr.forEach(element => {
+//         summed += element;
+//     });
     
-    console.log(summed);
-}
+//     console.log(summed);
+// }
 
 // sum([1,2,4,3,10]);
 
 // 2). Reversing an Array
 
-// function reverseArray(arr) {
-//     console.log(arr);
-// }
+function reverseArray(arr) {
+    let newArray = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+      newArray.push(arr[i]);
+    }
+    return newArray;
+  }
 
-// reverseArray([1,3,3,7]);
+reverseArray([1,3,3,7]);
 
-const arr = ["1", "2", "3", "4", "5"];
+// const arr = ["1", "2", "3", "4", "5"];
 
-const total = arr.reduceRight((prev, cur) => {
-    return prev + cur;
-});
+// const total = arr.reduceRight((prev, cur) => {
+//     return prev + cur;
+// });
 
-console.log(total);
+// console.log(total);
